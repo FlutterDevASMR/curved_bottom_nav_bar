2956c7058f7856dc9cd56c4f4c86fa40579cad8c
FlutterDevASMR <flutterdevasmr@gmail.com>
2023-07-13 12:04:10 +0330

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +11,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepOrange,
+      bottomNavigationBar: CurvedNavigationBar(
+        items: [
+          Icon(Icons.home),
+          Icon(Icons.search),
+          Icon(Icons.add),
+          Icon(Icons.person),
+          Icon(Icons.settings),
+        ],
+        backgroundColor: Colors.deepOrange,
+        color: Colors.deepOrange.shade100,
+        animationDuration: Duration(milliseconds: 300),
+      ),
     );
   }
 }
